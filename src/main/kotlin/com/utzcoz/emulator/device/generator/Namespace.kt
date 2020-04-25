a42a0e2c6a5dd0c458d335d9b6b04f3300c8742e
utzcoz <utzcoz@outlook.com>
2020-04-25 22:14:49 +0800

    Remove unused Namespace
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- src/main/kotlin/com/utzcoz/emulator/device/generator/Namespace.kt
+++
@@ -1,7 +0,0 @@
-package com.utzcoz.emulator.device.generator
-
-class Namespace {
-    companion object {
-        const val DEVICES_NAMESPACE_V5 = "http://schemas.android.com/sdk/devices/5"
-    }
-}
