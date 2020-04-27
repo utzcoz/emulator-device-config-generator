7326ae15b318d66c9d9976e8f1b447907fe09908
utzcoz <utzcoz@outlook.com>
2020-04-27 21:24:13 +0800

    Remove javafx
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- src/main/kotlin/com/utzcoz/emulator/device/generator/DeviceGenerator.kt
+++ src/main/kotlin/com/utzcoz/emulator/device/generator/DeviceGenerator.kt
@@ -1,27 +1,10 @@
 package com.utzcoz.emulator.device.generator
 
-import javafx.application.Application
-import javafx.fxml.FXMLLoader
-import javafx.scene.Parent
-import javafx.scene.Scene
-import javafx.stage.Stage
-import tornadofx.launch
-import java.io.File
-
-class DeviceGenerator : Application() {
-    override fun start(primaryStage: Stage) {
-        val scenePath = "${File.separator}layout${File.separator}scene.fxml"
-        val root = FXMLLoader.load<Parent>(DeviceGenerator::class.java.getResource(scenePath))
-        primaryStage.title = "Hello world"
-        primaryStage.scene = Scene(root, 300.0, 275.0)
-        primaryStage.show()
-    }
-}
-
-class Controller {
+class DeviceGenerator {
 
 }
+
 
 fun main(args: Array<String>) {
-    launch<DeviceGenerator>(args)
+    System.out.println("Hello world")
 }
